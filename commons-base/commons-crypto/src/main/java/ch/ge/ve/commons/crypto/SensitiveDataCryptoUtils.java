84f2030d87fe53fedb67d301cf0cb48593df734f
EdOverflow <EdOverflow@users.noreply.github.com>
2017-01-04 08:47:29 +0100

    Improve constantTimeArrayCompare() (#21)

--- commons-base/commons-crypto/src/main/java/ch/ge/ve/commons/crypto/SensitiveDataCryptoUtils.java
+++ commons-base/commons-crypto/src/main/java/ch/ge/ve/commons/crypto/SensitiveDataCryptoUtils.java
@@ -423,6 +423,10 @@ public class SensitiveDataCryptoUtils {
     }
 
     private static boolean constantTimeArrayCompare(byte[] a1, byte[] a2) {
+        if (a1.length != a2.length) {
+            return false;
+        }
+
         int result = 0;
         for (int i = 0; i < a1.length; i++) {
             result |= a1[i] ^ a2[i];
